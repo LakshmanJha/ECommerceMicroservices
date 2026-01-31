8334b51946b6984f4c70d8268a8111aca0fe4c67
LakshmanJha <lakshman.kumar@nethealth.com>
2026-02-01 01:49:32 +0530

    commit to Catlog

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,15 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+//Add Services to Container
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+var app = builder.Build();
+//Configure the HTTP request pipeline.
+
+app.MapCarter();
+app.Run();
